02e7dd99056035c46c1d2e4bea2ae1ca95516b7b
Anton Petrenko <petrenkoantserg@gmail.com>
2013-09-28 15:31:42 +0300

    added static dir

--- egonomist/core/settings.py
+++ egonomist/core/settings.py
@@ -71,9 +71,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    join(PROJECT_ROOT, 'static'),
 )
 
 # List of finder classes that know how to find static files in
