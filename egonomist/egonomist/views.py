b7a94148668c94a1a4aecfa0430d595c8f7bf271
Alexander Chekunkov <chekunkov@gmail.com>
2013-09-29 01:42:47 +0300

    pep8

--- egonomist/egonomist/views.py
+++ egonomist/egonomist/views.py
@@ -12,9 +12,10 @@ from .models import Photo
 
 
 api = InstagramAPI(
-        client_id=settings.INSTAGRAM_CLIENT_ID,
-        client_secret=settings.INSTAGRAM_CLIENT_SECRET,
-        redirect_uri=settings.LOGIN_REDIRECT_URL)
+    client_id=settings.INSTAGRAM_CLIENT_ID,
+    client_secret=settings.INSTAGRAM_CLIENT_SECRET,
+    redirect_uri=settings.LOGIN_REDIRECT_URL
+)
 
 
 def hello(request):
@@ -26,22 +27,29 @@ def complete(request):
     code = request.GET['code']
     access_token = api.exchange_code_for_access_token(code)
     auth_api = InstagramAPI(access_token=access_token[0])
-    user, _ = User.objects.get_or_create(username=access_token[1].get('username'))
+    user, _ = User.objects.get_or_create(
+        username=access_token[1].get('username')
+    )
 
     # Move to worker
     recent_media, _ = auth_api.user_recent_media(count=-1)
     for media in recent_media:
         photo, created = Photo.objects.get_or_create(
-            user = user,
-            instagram_id = media.id)
+            user=user,
+            instagram_id=media.id
+        )
         if created:
             img_temp = NamedTemporaryFile(delete=True)
             img_temp.write(
-                urllib2.urlopen(media.images['standard_resolution'].url).read())
+                urllib2.urlopen(
+                    media.images['standard_resolution'].url
+                ).read()
+            )
             img_temp.flush()
-
-            photo.image.save('{}.jpg'.format(photo.instagram_id), File(img_temp))
-
+            photo.image.save(
+                '{}.jpg'.format(photo.instagram_id),
+                File(img_temp)
+            )
     return redirect('choose')
 
 
