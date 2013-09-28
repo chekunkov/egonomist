6e042ec4a366363204af2c65524c12353e18e032
Anton Petrenko <petrenkoantserg@gmail.com>
2013-09-29 01:27:16 +0300

    image file deletion on Photo deletion

--- egonomist/egonomist/models.py
+++ egonomist/egonomist/models.py
@@ -6,3 +6,10 @@ class Photo(models.Model):
     user = models.ForeignKey(User, related_name='photos')
     instagram_id = models.CharField(max_length=32)
     image = models.ImageField(upload_to='photos')
+
+    def __unicode__(self):
+        return self.instagram_id
+
+    def delete(self, *args, **kwargs):
+        self.image.delete()
+        super(Photo, self).delete(*args, **kwargs)
