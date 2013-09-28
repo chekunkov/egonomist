0375dd01c2f6c5dd37126a600e967804a4ac9276
Ivan Dyadin <me@qntzr.com.ua>
2013-09-28 19:55:51 +0300

    Select and save to array photo id.

---
+++ egonomist/static/js/main.js
@@ -0,0 +1,19 @@
+var ids = [];
+
+$(function() {
+	$( ".select-button" ).click(function() {
+		if ($(this).hasClass('selected')) {
+			$(this).html("Select").removeClass("selected ").addClass("btn-info");
+			var removeItem = $(this).data("id");   // item do array que deverÃ¡ ser removido
+			 
+			ids = jQuery.grep(ids, function(value) {
+        		return value != removeItem;
+        	});
+        	console.log(ids);
+		} else {
+			$(this).html("Selected <span class='glyphicon white glyphicon-ok'></span>").addClass("selected btn-success").removeClass("btn-info");
+			ids.push($(this).data("id"));
+			console.log(ids);
+		}
+	});
+});
